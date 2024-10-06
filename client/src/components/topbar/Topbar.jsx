87057b898ea4d2f07eb8dc4d0f7963e3f63518c3
nayan-ray <niranzanroy1@gmail.com>
2024-10-06 14:38:30 +0600

    header update

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -2,53 +2,78 @@ import React from 'react'
 import "./topbar.css"
 import logo from "../../assets/images/ostadlogo.png";
 import search from "../../assets/images/search.svg"
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
 
 const Topbar = () => {
   return (
     <div className='topBar'>
           <div className="topBar-wrapper">
                <div className="topBar-left">
-                    <img className='topBar-logo' src={logo} alt="" />
-                    <div className="searchBar">
-                        <img src={search} alt="" /> 
-                        কোর্স সার্চ করুন
-                    </div>
-                    <button>
-                      <img alt="special-icon"  src="https://cdn.ostad.app/public/upload/2024-03-20T04-42-25.711Z-material-symbols_auto-awesome.png" />
+                     <img className='d-none' src="https://cdn.ostad.app/public/upload/2024-03-10T05-11-30.796Z-single-logo.svg" alt="" />
 
-                       স্পেশাল অফার
-                    </button>
-                    <span className="topBar-item">
-                          ফ্রী কোর্সেস
-                    </span>
-                    <span className="topBar-item">
-                           ব্লগ
-                    </span>
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                              ডাউনলোড
-                        </button>
-                        <ul class="dropdown-menu">
-                             <li><a class="dropdown-item" href="#">Action</a></li>
-                             <li><a class="dropdown-item" href="#">Another action</a></li>
-                             <li><a class="dropdown-item" href="#">Something else here</a></li>
-                       </ul>
-                    </div>
+                     <img className='topBar-logo ' src={logo} alt="" />
+
+                    <div className="topBar-items">
+                       <div className="searchBar">
+                          <img className='search-icon' src={search} alt="" /> 
+                           কোর্স সার্চ করুন
+                       </div>
+                       <div className="topbar-item-lists">
+
+                           <button className='special-button'>
+                              <img alt="special-icon" className='special-icon'  src="https://cdn.ostad.app/public/upload/2024-03-20T04-42-25.711Z-material-symbols_auto-awesome.png" />
+
+                               স্পেশাল অফার
+                          </button>
+                          <span className="topBar-item">
+                                  কোড মামা
+                          </span>
+
+                          <span className="topBar-item">
+                                  ফ্রী কোর্সেস
+                          </span>
+
+
+                          <span className="topBar-item">
+                              ব্লগ
+                          </span>
+                          <div class="dropdown">
+                             <button class="btn topBar-item " type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                 ডাউনলোড
+                                 <KeyboardArrowDownOutlinedIcon className='arrow-down-icon'/>
+                             </button>
+                             <ul class="dropdown-menu">
+                                 <li><a class="dropdown-item" href="#">Action</a></li>
+                                 <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                          </div>
+
+
+                       </div>
+
+                  </div>
+                    
 
                </div>
                <div className="topBar-right">
-                       <button type="button" class="btn btn-primary position-relative rounded-circle">
-                            Inbox
-                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                                       99+
-                               <span class="visually-hidden">unread messages</span>
-                            </span>
-                      </button>
-                      <div className="right-item">
-                           <img  src="https://cdn.ostad.app/public/upload/2023-08-06T04-12-22.324Z-imageE66.png" />
+                      <div className='topBar-right-item notification'>
+                           <NotificationsNoneOutlinedIcon />
+                          <span  className="notification-count">
+
+                              20  
+                          </span>  
+                      </div>
+                      <div className="topBar-right-item language">
+                           <img className='lang-img'  src="https://cdn.ostad.app/public/upload/2023-08-06T04-12-22.324Z-imageE66.png" alt='language'/>
                            EN
                       </div>
-                      <div className="login">
+                      <div className="topBar-right-item course">
+                          সব কোর্স
+                          <KeyboardArrowDownOutlinedIcon className=' course-icon'/>
+                      </div>
+                      <div className="topBar-right-item login">
                         login
                       </div>
                </div>
